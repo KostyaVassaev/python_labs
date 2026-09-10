4d9cb198c7bb5806b51f7ce8f5b7030cfcf00111
KonstantinVass <vassaevka@gmail.com>
2026-09-10 17:10:57 +0300

    lab1 most done

---
+++ src/lab01/01_greeting.py
@@ -0,0 +1,3 @@
+name = input("Имя: ")
+old = int(input("Возраст: "))
+print(f"Привет, {name}! Через год тебе будет {old + 1}.")
---
+++ src/lab01/03_discount_vat.py
@@ -0,0 +1,16 @@
+price = float(input(""))
+discount = float(input(""))
+vat = float(input(""))
+
+base = price * (1 - discount/100)
+vat_amount = base * (vat/100)
+total = base + vat_amount
+
+prints = [
+    ("База после скидки:", base),
+    ("НДС:", vat_amount),
+    ("Итого к оплате:", total)
+]
+
+for text, val in prints:
+    print(f"{text:<18} {val:.2f}")
