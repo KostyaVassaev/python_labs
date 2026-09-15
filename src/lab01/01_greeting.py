41bb2c732beb5a4033a6850c089f5557a0cd8870
KonstantinVass <vassaevka@gmail.com>
2026-09-15 11:32:59 +0300

    better lab01/ex1

--- src/lab01/01_greeting.py
+++ src/lab01/01_greeting.py
@@ -1,3 +1,9 @@
-name = input("Имя: ")
-old = int(input("Возраст: "))
+name = ""
+while not name:
+    name = input("Имя: ")
+    if not name: print("Имя не введено, введите имя!")
+old = -1
+while old < 0:
+    old = int(input("Возраст: "))
+    if old < 0: print("Возраст не введён или введён некорректно, введите возраст!")
 print(f"Привет, {name}! Через год тебе будет {old + 1}.")
