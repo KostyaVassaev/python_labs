47421fd6ed6e1f63b225638f14f94b308c420f0f
KonstantinVass <vassaevka@gmail.com>
2026-09-15 12:00:03 +0300

    better lab01/ex1

--- src/lab01/01_greeting.py
+++ src/lab01/01_greeting.py
@@ -2,8 +2,9 @@ name = ""
 while not name or " " in name:
     name = input("Имя: ")
     if not name or " " in name: print("Имя не введено или введено некорректно, введите имя!")
-old = -1
-while old < 0:
-    old = int(input("Возраст: "))
-    if old < 0: print("Возраст не введён или введён некорректно, введите возраст!")
-print(f"Привет, {name}! Через год тебе будет {old + 1}.")
+old = "-1"
+while int(old) < 0:
+    old = input("Возраст: ").replace(",", ".")
+    if old < 0 or not old.isdigit() or "." in old:
+        print("Возраст не введён или введён некорректно, введите возраст (натуальное число или ноль)!")
+print(f"Привет, {name}! Через год тебе будет {int(old) + 1}.")
