8601f3f386ee430cd07416d4c1ea2e7dc1def52d
KonstantinVass <vassaevka@gmail.com>
2026-09-15 13:31:39 +0300

    rename function: to_n -> simple_nums_to_nsys

--- src/lib/conv_to_n.py
+++ src/lib/conv_to_n.py
@@ -1,6 +1,6 @@
-def to_n(a, sys):
-    if a < 0: raise ValueError("Только положительные числа")
-    if sys > 26 or sys < 2: raise ValueError("Некорректное основание системы")
+def simple_nums_to_n(a, sys):
+    if a < 0: raise ValueError("This function converts only non-negative numbers")
+    if sys > 26 or sys < 2: raise ValueError("Incorrect base of the number system")
     alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
     res = ""
     while a > 0:
