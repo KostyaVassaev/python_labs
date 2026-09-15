087913be8bbfe054fbe363d2425a6816b1656ff1
KonstantinVass <vassaevka@gmail.com>
2026-09-15 13:36:25 +0300

    base of the number system fix

--- src/lib/conv_to_n.py
+++ src/lib/conv_to_n.py
@@ -1,6 +1,6 @@
 def simple_nums_to_nsys(a, sys):
     if a < 0: raise ValueError("This function converts only non-negative numbers")
-    if sys > 26 or sys < 2: raise ValueError("Incorrect base of the number system")
+    if sys > 36 or sys < 2: raise ValueError("Incorrect base of the number system")
     alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
     res = ""
     while a > 0:
