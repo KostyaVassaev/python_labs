3251b48041f277c19004a795f84d64280a596a62
KonstantinVass <vassaevka@gmail.com>
2026-09-15 13:27:14 +0300

    lab01/ex4 optimization

--- src/lab01/04_minutes_to_hhmm.py
+++ src/lab01/04_minutes_to_hhmm.py
@@ -1,8 +1,7 @@
-a = "-1"
-while True:
+a = "."
+while not a.isdigit():
     a = input("Минуты: ")
     if not a.isdigit(): print("Введите положительное целое число минут!")
-    else: break
 
 m = int(a)
 print(f"{m//60:02}:{m%60:02}")
