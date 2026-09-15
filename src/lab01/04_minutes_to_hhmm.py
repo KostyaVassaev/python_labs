f3155cc621e59cac8aea4851b231c76442a8fc24
KonstantinVass <vassaevka@gmail.com>
2026-09-15 13:17:31 +0300

    better lab01/ex4

--- src/lab01/04_minutes_to_hhmm.py
+++ src/lab01/04_minutes_to_hhmm.py
@@ -1,2 +1,9 @@
-m = abs(int(input("Минуты: ")))
+a = "-1"
+while True:
+    a = input("Минуты: ")
+    if not a.isdigit(): print("Введите положительное целое число минут!")
+    elif int(a) < 0: print("Введите положительное целое число минут!")
+    else: break
+
+m = int(a)
 print(f"{m//60:02}:{m%60:02}")
